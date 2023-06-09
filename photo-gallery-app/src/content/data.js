60f46d94f397e8f60dae5dc141f7d6c9b41ff31f
John Obansa <johnnie.vehe@gmail.com>
2023-06-09 06:05:55 +0000

    initializing project

---
+++ photo-gallery-app/src/content/data.js
@@ -0,0 +1,65 @@
+export const data = [
+  {
+    "name": "Photo 1",
+    "url": "/images/image (1).jpg",
+    "description": "Photo 1 description",
+    "date": "2020-01-01",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 2",
+    "url": "/images/image (2).jpg",
+    "description": "Photo 2 description",
+    "date": "2020-01-02",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 3",
+    "url": "/images/image (3).jpg",
+    "description": "Photo 3 description",
+    "date": "2020-01-03",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 4",
+    "url": "/images/image (4).jpg",
+    "description": "Photo 4 description",
+    "date": "2020-01-04",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 5",
+    "url": "/images/image (5).jpg",
+    "description": "Photo 5 description",
+    "date": "2020-01-05",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 6",
+    "url": "/images/image (6).jpg",
+    "description": "Photo 6 description",
+    "date": "2020-01-06",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 7",
+    "url": "/images/image (7).jpg",
+    "description": "Photo 7 description",
+    "date": "2020-01-07",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 8",
+    "url": "/images/image (8).jpg",
+    "description": "Photo 8 description",
+    "date": "2020-01-08",
+    "type": "image/jpg",
+  },
+  {
+    "name": "Photo 9",
+    "url": "/images/image (9).jpg",
+    "description": "Photo 9 description",
+    "date": "2020-01-09",
+    "type": "image/jpg",
+  }
+]
